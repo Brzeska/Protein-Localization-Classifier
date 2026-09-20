371c32f6e29a17a847e9215ac0ebfe16c6765704
Phillip Brandel <pdbrandel@gmail.com>
2026-09-20 00:19:26 -0700

    made classifier.py output legible

--- classifier.py
+++ classifier.py
@@ -16,16 +16,28 @@ lysosome_vacuole_trigram = pickle.load(open('lysosome_vacuole_trigram.pkl','rb')
 golgi_apparatus_trigram = pickle.load(open('golgi_apparatus_trigram.pkl','rb'))
 peroxisome_trigram = pickle.load(open('peroxisome_trigram.pkl','rb'))
 
-#ubiquitin
-sequence = 'MAAGGAEGGPGPSAAMGDCAEIKSQFRTREGFYKLLPGDATRRSGPTSAQTPAPPQPTQPPPGPAAASGPGAAGPASSPPPAGPGPGPALPAVRLSLVRLGDPDGAGEPPSTPSGLGAGGDRVCFNLGRELYFYPGCCRSGSQRSIDLNKPIDKRIYKGTQPTCHDFNQFTAATETISLLVGFSAGQVQYLDLIKKDTSKLFNEERLIDKTKVTYLKWLPESESLFLASHASGHLYLYNVSHPCTSTPPQYSLLKQGEGFAVYAAKSKAPRNPLAKWAVGEGPLNEFAFSPDGRHLACVSQDGCLRVFHFDSMLLRGLMKSYFGGLLCVCWSPDGRYVVTGGEDDLVTVWSFTEGRVVARGHGHKSWVNAVAFDPYTTRAEEAASASADGDPSGEEEEPEVTSSDTGAPVSPLPKAGSITYRFGSAGQDTQFCLWDLTEDVLSPHPSLARTRTLPGTPGATPPASGSSRAGETGAGPLPRSLSRSNSLPHPAGGGKAGGPSASMEPGIPFSIGRFATLTLQERRDRGAEKEHKRYHSLGNISRGGSGGNSSNDKLSGPAPRSRLDPAKVLGTALCPRIHEVPLLEPLVCKKIAQERLTVLLFLEDCIITACQEGLICTWARPGKAFTDEETEAQAGQASWPRSPSKSVVEGISSQPGSSPSGTVV'
+#put sequence here
+sequence = 'MKALIVLGLVLLSVTVQGKVFERCELARTLKRLGMDGYRGISLANWMCLAKWESGYNTRATNYNAGDRSTDYGIFQINSRYWCNDGKTPGAVNACHLSCSALLQDNIADAVACAKRVVRDPQGIRAWVAWRNRCQNRDVRQYVQGCGV'
 
-print(cytoplasm_trigram.compute_loss(sequence))
-print(nucleus_trigram.compute_loss(sequence))
-print(extracellular_trigram.compute_loss(sequence))
-print(cellmembrane_trigram.compute_loss(sequence))
-print(mitochondrion_trigram.compute_loss(sequence))
-print(plastid_trigram.compute_loss(sequence))
-print(endoplasmicreticulum_trigram.compute_loss(sequence))
-print(lysosome_vacuole_trigram.compute_loss(sequence))
-print(golgi_apparatus_trigram.compute_loss(sequence))
-print(peroxisome_trigram.compute_loss(sequence))
+losses = [cytoplasm_trigram.compute_loss(sequence),
+    nucleus_trigram.compute_loss(sequence),
+    extracellular_trigram.compute_loss(sequence),
+    cellmembrane_trigram.compute_loss(sequence),
+    mitochondrion_trigram.compute_loss(sequence),
+    plastid_trigram.compute_loss(sequence),
+    endoplasmicreticulum_trigram.compute_loss(sequence),
+    lysosome_vacuole_trigram.compute_loss(sequence),
+    golgi_apparatus_trigram.compute_loss(sequence),
+    peroxisome_trigram.compute_loss(sequence)]
+
+minimum_index = 0
+minimum = losses[minimum_index]
+
+for i in range(1,len(losses)):
+    if losses[i] < minimum:
+        minimum = losses[i]
+        minimum_index = i
+
+labels = ['cytoplasm','nucleus','extracellular','cell membrane','mitochondrion','plastid','endoplasmic reticulum','lysosome/vacuole','golgi apparatus','peroxisome']
+
+print(f'Predicted localization is: {labels[minimum_index]}!')
